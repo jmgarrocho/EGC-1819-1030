8c2c89bb22ea4ba0a4b5b91487a3a13cd9d5c395
MrGaRRos <juanmagarrocho@gmail.com>
2020-11-30 18:48:05 +0100

    Error corregido

--- decide/local_settings.example.py
+++ decide/local_settings.example.py
@@ -30,8 +30,8 @@ BASEURL = 'http://10.5.0.1:8000'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
+        'NAME': 'decide',
+        'USER': 'decide',
         'HOST': 'db',
         'PORT': '5432',
     }
